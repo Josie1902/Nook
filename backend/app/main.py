7ff86b826602d8d7abf2cbf71642a5ce80b48c90
Josie1902 <theofficialjosie@gmail.com>
2026-08-31 00:00:28 +0800

    feat (auth): add router to check health

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-
+from app.presentation.api.health import router as health_router
 from app.presentation.api.users import router as user_router
 
 app = FastAPI()
@@ -18,4 +18,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(health_router)
 app.include_router(user_router)
