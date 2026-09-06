2420c98947f544ff0126a5f2ac06b31ca62b437c
Josie1902 <theofficialjosie@gmail.com>
2026-09-06 21:13:30 +0800

    chore(backend): delete files

--- backend/app/application/processing/pdf_processor.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from app.domain.entities.book import Book
-
-
-class PdfProcessor(ABC):
-    @abstractmethod
-    def process(self, book: Book) -> None:
-        ...
